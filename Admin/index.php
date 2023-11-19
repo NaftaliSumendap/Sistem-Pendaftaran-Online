d37bce8dccf4a1b898d40b4c12683509314b7918
NaftaliSumendap <132432392+NaftaliSumendap@users.noreply.github.com>
2023-11-20 00:15:39 +0800

    +admin page

---
+++ Admin/index.php
@@ -0,0 +1,61 @@
+<?php
+require '../SCRIPT/functions.php';
+$users = query("SELECT * FROM users");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!--Fonts-->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300&family=Pacifico&display=swap" rel="stylesheet" />
+
+    <script src="https://unpkg.com/feather-icons"></script>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Pendaftaran</title>
+    <link rel="stylesheet" href="style.css" />
+    <script src="SCRIPT/script.js"></script>
+</head>
+
+<body>
+    <main>
+        <div>
+            <h1>Daftar User</h1>
+        </div>
+        <div>
+            <table border="1" cellpading="10" cellspacing="0">
+                <tr>
+                    <th>No.</th>
+                    <th>Ubah Data</th>
+                    <th>ID</th>
+                    <th>Nama</th>
+                    <th>Nomor</th>
+                    <th>Email</th>
+                </tr>
+
+                <?php $i = 1; ?>
+                <?php foreach ($users as $row) : ?>
+                    <tr>
+                        <td><?= $i; ?></td>
+                        <td>
+                            <a href="">Update</a> |
+                            <a href="">Delete</a>
+                        </td>
+                        <td><?= $row["id"]; ?></td>
+                        <td><?= $row["nama"]; ?></td>
+                        <td><?= $row["nomor"]; ?></td>
+                        <td><?= $row["email"]; ?></td>
+                    </tr>
+                    <?php $i++; ?>
+                <?php endforeach; ?>
+            </table>
+        </div>
+        <!-- Feather Icons-->
+        <script>
+            feather.replace();
+        </script>
+</body>
+
+</html>
